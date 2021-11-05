6eb22323a9dc2402f7575626c10f798f3dfb668c
putrapuices <puices14@gmail.com>
2021-11-05 11:46:10 +0700

    setting routes diserver.js

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const app = express();
 // parse apllication/json   
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+//pangil routes
+var routes = require('./routes');
+routes(app);
+
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
+
+
