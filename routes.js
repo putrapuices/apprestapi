d21a5ecd243536071094b8402ccf7cb2dbbcc33a
putrapuices <puices14@gmail.com>
2021-11-05 15:06:33 +0700

    Melakukan hapus data berdasarkan id

--- routes.js
+++ routes.js
@@ -1,22 +1,17 @@
-'use strict';
+"use strict";
 
+module.exports = function (app) {
+  var jsonku = require("./controller");
 
+  app.route("/").get(jsonku.index);
 
-module.exports = function (app){
-    var jsonku = require('./controller');
+  app.route("/tampil").get(jsonku.tampilsemuamahasiswa);
 
-    app.route('/')
-    .get(jsonku.index);
+  app.route("/tampil/:id").get(jsonku.tampilberdasarkanid);
 
-    app.route('/tampil')
-        .get(jsonku.tampilsemuamahasiswa);
+  app.route("/tambah").post(jsonku.tambahMahasiswa);
 
-    app.route('/tampil/:id')
-    .get(jsonku.tampilberdasarkanid);
+  app.route("/ubah").put(jsonku.ubahMahasiswa);
 
-    app.route('/tambah')
-    .post(jsonku.tambahMahasiswa);
-
-    app.route('/ubah')
-        .put(jsonku.ubahMahasiswa);
+  app.route("/hapus").delete(jsonku.hapusMahasiswa);
 };
